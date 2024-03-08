1cb6cc87b442e6cf6a309164dbcfdb798cce7d10
Joseph Reynolds <72422816+JosephJamesReynolds@users.noreply.github.com>
2024-03-07 21:05:38 -0800

    Adds Navbar

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -4,13 +4,13 @@ import { setProvider, setNetwork, setAccount } from "./reducers/provider";
 
 import { setContracts, setSymbols, balancesLoaded } from "./reducers/tokens";
 
-import { setContract } from "./reducers/amm";
+import { setContract, sharesLoaded } from "./reducers/amm";
 
 import TOKEN_ABI from "../abis/Token.json";
 import AMM_ABI from "../abis/AMM.json";
 import config from "../config.json";
 
-export const loadProvider = async (dispatch) => {
+export const loadProvider = (dispatch) => {
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   dispatch(setProvider(provider));
 
@@ -60,16 +60,24 @@ export const loadAMM = async (provider, chainId, dispatch) => {
     provider
   );
 
-  dispatch(setContract([amm]));
+  dispatch(setContract(amm));
 
   return amm;
 };
 
 // ----------------------------------------------------------------
 // LOAD BALANCES AND SHARES
-export const loadBalances = async (tokens, account, dispatch) => {
+export const loadBalances = async (amm, tokens, account, dispatch) => {
   const balance1 = await tokens[0].balanceOf(account);
   const balance2 = await tokens[1].balanceOf(account);
 
-  dispatch(balancesLoaded(balance1, balance2));
+  dispatch(
+    balancesLoaded([
+      ethers.utils.formatUnits(balance1.toString(), "ether"),
+      ethers.utils.formatUnits(balance2.toString(), "ether"),
+    ])
+  );
+
+  const shares = await amm.shares(account);
+  dispatch(sharesLoaded(ethers.utils.formatUnits(shares.toString(), "ether")));
 };
