c841248588f2697a97706b4e8c027dff5cb3263e
Joseph Reynolds <72422816+JosephJamesReynolds@users.noreply.github.com>
2024-03-23 20:50:49 -0700

    Add Withdraw

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -10,6 +10,9 @@ import {
   depositRequest,
   depositSuccess,
   depositFail,
+  withdrawRequest,
+  withdrawSuccess,
+  withdrawFail,
   swapRequest,
   swapSuccess,
   swapFail,
@@ -125,6 +128,23 @@ export const addLiquidity = async (
     dispatch(depositFail());
   }
 };
+
+// -----------------------------------------------------//
+// Remove Liquidity
+export const removeLiquidity = async (provider, amm, shares, dispatch) => {
+  try {
+    dispatch(withdrawRequest());
+    const signer = await provider.getSigner();
+
+    let transaction = await amm.connect(signer).removeLiquidity(shares);
+    await transaction.wait();
+
+    dispatch(withdrawSuccess(transaction.hash));
+  } catch (error) {
+    dispatch(withdrawFail());
+  }
+};
+
 // -----------------------------------------------------// Swap
 
 export const swap = async (provider, amm, token, symbol, amount, dispatch) => {
